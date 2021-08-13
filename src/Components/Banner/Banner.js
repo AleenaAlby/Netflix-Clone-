ffecb7cd4d6584e0bfa4f777ecca92bb390d0b62
Aleena Alby <aleenaalby20@gmail.com>
2021-08-13 11:12:44 +0530

    First Commit

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Banner.css"
+
+function Banner() {
+    return (
+        <div className="banner">
+            <div className="content">
+                <h1 className="title">Movie Name</h1>
+                <div className="banner-buttons">
+                    <button className="button">Play</button>
+                    <button className="button">My List</button>
+                </div>
+                <h1 className="description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat dicta porro corporis? 
+                Dolorum commodi est cumque quo voluptate nobis, </h1>
+            </div>  
+            <div className="fade-bottom"></div>    
+        </div>
+    )
+}
+
+export default Banner
