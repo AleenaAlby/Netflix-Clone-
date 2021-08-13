b4cb0e44e188234c0999d0f12d4990b8e88d6b96
Aleena Alby <aleenaalby20@gmail.com>
2021-08-13 15:25:19 +0530

    TMDB added

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import "./Banner.css"
+import {API_KEY} from '../../constants/constants'
+import axios from '../../axios'
+import {imageUrl} from '../../constants/constants'
 
 function Banner() {
+
+    const [movie, setMovie] = useState()
+
+    useEffect(() => {
+        axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`)
+        .then((response) =>{
+                // console.log(response.data.results[0])
+                setMovie(response.data.results[1] )
+            }
+        )
+    }, [])
+
     return (
-        <div className="banner">
+        <div 
+        style = {{backgroundImage: `url(${movie ? imageUrl+movie.backdrop_path : ""})`}}
+        className="banner">
             <div className="content">
-                <h1 className="title">Movie Name</h1>
+                <h1 className="title">{movie ? movie.title : ""}</h1>
                 <div className="banner-buttons">
                     <button className="button">Play</button>
                     <button className="button">My List</button>
                 </div>
-                <h1 className="description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat dicta porro corporis? 
-                Dolorum commodi est cumque quo voluptate nobis, </h1>
+                <h1 className="description">{movie ? movie.overview : ""} </h1>
             </div>  
             <div className="fade-bottom"></div>    
         </div>
